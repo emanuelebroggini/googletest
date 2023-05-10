8617e8ae376f35ebf3c0360201bab0f709d022be
leobeltra <99175159+leobeltra@users.noreply.github.com>
2023-05-10 13:54:46 +0200

    Create lele.cpp
    Andrea Lio e Fude

---
+++ googletest/samples/lele.cpp
@@ -0,0 +1,10 @@
+int main() {
+    double fude = 0.5;
+    double andre = 1.5;
+    int lio = 6;
+
+    double res = fude + andre + (double)lio;
+
+    std::cout << "Il risultato di questi 3 idioti è " << res << std::endl;
+    return 0;
+}
