be37a7c10a40d7736590700c67bedb359d42850d
leobeltra <leonardobeltrame00@gmail.com>
2023-05-26 22:13:33 +0200

    bin/bash with right path
    lele

--- scripts/Linux-Build.sh
+++ scripts/Linux-Build.sh
@@ -1,2 +1,3 @@
+#!/bin/bash
 cd /var/lib/jenkins/workspace/googletest_main/googletest/samples
 g++ -o sample1_unittest sample1_unittest.cc sample1.cc -lgtest -lpthread
--- scripts/Linux-Run.sh
+++ scripts/Linux-Run.sh
@@ -1,2 +1,3 @@
+#!/bin/bash
 cd /var/lib/jenkins/workspace/googletest_main/googletest/samples
 ./sample1_unittest
