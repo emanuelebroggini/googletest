28bb4966f2c5761d85259b635ebd2a9c7857990a
leobeltra <leonardobeltrame00@gmail.com>
2023-05-26 21:57:18 +0200

    bin/bash added...
    lele

--- scripts/Linux-Build.sh
+++ scripts/Linux-Build.sh
@@ -1,2 +1,3 @@
+#!/bin/bash
 cd /usr/src/googletest/googletest/samples/
 g++ -o sample1_unittest sample1_unittest.cc sample1.cc -lgtest -lpthread
--- scripts/Linux-Run.sh
+++ scripts/Linux-Run.sh
@@ -1,2 +1,3 @@
+#!/bin/bash
 cd /usr/src/googletest/googletest/samples/
 ./sample1_unittest
