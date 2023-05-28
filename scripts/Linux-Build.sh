b5e91a5831a790b4776814f264128127105e6ba2
Andrea Ortenzi <96396864+AndreaOrtenzi@users.noreply.github.com>
2023-05-28 23:13:38 +0200

    Update Linux-Build.sh
    changed linked libraries

--- scripts/Linux-Build.sh
+++ scripts/Linux-Build.sh
@@ -1,1 +1,1 @@
-cd googletest && cd samples && g++ -o sample1_unittest sample1.cc -pthread -lgtest -lgtest_main
+cd googletest && cd samples && g++ -o sample1_unittest sample1_unittest.cc sample1.cc -lgtest_main -lgtest -lpthread
