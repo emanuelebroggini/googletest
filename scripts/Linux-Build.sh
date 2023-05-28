2d82585538d0aa65ceb076906081c085bbb34ac2
Andrea Ortenzi <96396864+AndreaOrtenzi@users.noreply.github.com>
2023-05-29 00:18:45 +0200

    Libraries path specified in g++ command

--- scripts/Linux-Build.sh
+++ scripts/Linux-Build.sh
@@ -1,1 +1,1 @@
-cd googletest && cd samples && g++ -o sample1_unittest sample1_unittest.cc sample1.cc /usr/lib/x86_64-linux-gnu/libpthread.a -lgtest_main -lgtest
+cd googletest && cd samples && g++ -o sample1_unittest sample1_unittest.cc sample1.cc -L/usr/lib/x86_64-linux-gnu/ -lpthread -lgtest_main -lgtest
