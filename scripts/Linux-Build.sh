eebc8a326296dbbd07d0847c46a273e7c5709ff0
AndreaOrtenzi <andre.orto@tiscali.it>
2023-05-28 12:11:05 +0200

    lpthread changed in pthread flag

--- scripts/Linux-Build.sh
+++ scripts/Linux-Build.sh
@@ -1,1 +1,1 @@
-cd googletest && cd samples && g++ -o sample1_unittest sample1_unittest.cc sample1.cc -lgtest -lpthread
+cd googletest && cd samples && g++ -o sample1_unittest sample1_unittest.cc sample1.cc -lgtest -pthread
