e0dfe40348cbd750d87270b5101c2b667a17c1d9
AndreaOrtenzi <andre.orto@tiscali.it>
2023-05-28 12:43:49 +0200

    try to use sample main

--- scripts/Linux-Build.sh
+++ scripts/Linux-Build.sh
@@ -1,1 +1,1 @@
-cd googletest && cd samples && g++ -std=c++11 -o sample1_unittest -pthread -lgtest sample1_unittest.cc sample1.cc
+cd googletest && cd samples && g++ -std=c++11 -o sample1_unittest -pthread -lgtest ../src/gtest_mail.cc sample1_unittest.cc sample1.cc
