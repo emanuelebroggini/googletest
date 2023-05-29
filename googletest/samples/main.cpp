fb7713e8fececea925d4b5788ea496fda25065be
emanuelebroggini <emanuele.broggini@mail.polimi.it>
2023-05-29 11:52:35 +0200

    Create main.cpp
    Added un main un po' a caso

---
+++ googletest/samples/main.cpp
@@ -0,0 +1,58 @@
+#include "sample1.h"
+#include "sample2.h"
+#include "sample3.h"
+#include "sample4.h"
+
+#include <limits.h>
+
+#include "gtest/gtest.h"
+
+namespace {
+
+TEST(FactorialTest, Negative) {
+  // This test is named "Negative", and belongs to the "FactorialTest"
+  // test case.
+  EXPECT_EQ(1, Factorial(6));
+  EXPECT_EQ(1, Factorial(2));
+  EXPECT_GT(Factorial(-10), 0);
+
+  // <TechnicalDetails>
+  //
+  // EXPECT_EQ(expected, actual) is the same as
+  //
+  //   EXPECT_TRUE((expected) == (actual))
+  //
+  // except that it will print both the expected value and the actual
+  // value when the assertion fails.  This is very helpful for
+  // debugging.  Therefore in this case EXPECT_EQ is preferred.
+  //
+  // On the other hand, EXPECT_TRUE accepts any Boolean expression,
+  // and is thus more general.
+  //
+  // </TechnicalDetails>
+}
+// Tests some trivial cases.
+TEST(IsPrimeTest, Trivial) {
+  EXPECT_FALSE(IsPrime(0));
+  EXPECT_FALSE(IsPrime(1));
+  EXPECT_TRUE(IsPrime(2));
+  EXPECT_TRUE(IsPrime(3));
+}
+
+TEST(Counter, Increment) {
+  Counter c;
+
+  // Test that counter 0 returns 0
+  EXPECT_EQ(0, c.Decrement());
+
+  // EXPECT_EQ() evaluates its arguments exactly once, so they
+  // can have side effects.
+
+  EXPECT_EQ(0, c.Increment());
+  EXPECT_EQ(1, c.Increment());
+  EXPECT_EQ(2, c.Increment());
+
+  EXPECT_EQ(3, c.Decrement());
+}
+
+}
