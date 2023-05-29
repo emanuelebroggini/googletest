459fa61b8258ce3f14781f0de6192338c85fd885
emanuelebroggini <emanuele.broggini@mail.polimi.it>
2023-05-29 13:18:05 +0200

    lele
    rats

--- scripts/Linux-Build.sh
+++ scripts/Linux-Build.sh
@@ -1,1 +1,3 @@
-cd googletest && cd samples && g++ -o sample1_unittest sample1_unittest.cc sample1.cc /usr/lib/x86_64-linux-gnu/libpthread.a -lgtest_main /usr/lib/x86_64-linux-gnu/libgtest.a
+cd googletest && cd samples && g++ -o sample1_unittest sample1_unittest.cc sample1.cc -lpthread -lgtest_main -lgtest
+
+
