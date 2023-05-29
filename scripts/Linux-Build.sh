1c127885391d7df6d9df939af6f81432c3c0d675
leobeltra <leonardobeltrame00@gmail.com>
2023-05-29 11:56:48 +0200

    return to origin

--- scripts/Linux-Build.sh
+++ scripts/Linux-Build.sh
@@ -1,1 +1,1 @@
-cd googletest && cd samples && g++ -o sample1_unittest sample1_unittest.cc sample1.cc /usr/lib/x86_64-linux-gnu/libpthread.a /usr/src/gtest/lib/libgtest_main.a /usr/src/gtest/lib/libgtest.a
+cd googletest && cd samples && g++ -o sample1_unittest sample1_unittest.cc sample1.cc -lpthread /usr/src/gtest/lib/libgtest_main.a /usr/src/gtest/lib/libgtest.a
