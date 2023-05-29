d5e88c88d0b8c657c58c284f19fcd47e43be5994
leobeltra <leonardobeltrame00@gmail.com>
2023-05-29 15:53:35 +0200

    MARCHERO WORK
    Lele

--- scripts/Linux-Build.sh
+++ scripts/Linux-Build.sh
@@ -1,3 +1,3 @@
-export LD_LIBRARY_PATH="/usr/lib/x86_64-linux-gnu/" && cd googletest && cd samples && g++ -o sample1_unittest sample1_unittest.cc sample1.cc -lpthread -lgtest_main -lgtest
+cd googletest && cd samples && g++ -o sample1_unittest sample1_unittest.cc sample1.cc -lpthread -lgtest_main -lgtest
 
 
