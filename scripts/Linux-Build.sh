69c3b28715c8dafb52423da836367a3f7382b3f1
emanuelebroggini <emanuele.broggini@mail.polimi.it>
2023-05-29 13:24:53 +0200

    ratforreal
    really

--- scripts/Linux-Build.sh
+++ scripts/Linux-Build.sh
@@ -1,3 +1,3 @@
-cd googletest && cd samples && g++ -o sample1_unittest sample1_unittest.cc sample1.cc -lpthread -lgtest_main -lgtest
+export LD_LIBRARY_PATH="/usr/lib/x86_64-linux-gnu/" && cd googletest && cd samples && g++ -o sample1_unittest sample1_unittest.cc sample1.cc -lpthread -lgtest_main -lgtest
 
 
