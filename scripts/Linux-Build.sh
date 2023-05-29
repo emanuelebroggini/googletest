8d10f8227057d55fbecddfdfd3ce5415a14617a4
emanuelebroggini <emanuele.broggini@mail.polimi.it>
2023-05-29 13:11:00 +0200

    Change order

--- scripts/Linux-Build.sh
+++ scripts/Linux-Build.sh
@@ -1,1 +1,1 @@
-cd googletest && cd samples && g++ -o sample1_unittest sample1_unittest.cc sample1.cc /usr/lib/x86_64-linux-gnu/libpthread.a /usr/lib/x86_64-linux-gnu/libgtest.a -lgtest_main
+cd googletest && cd samples && g++ -o sample1_unittest sample1_unittest.cc sample1.cc /usr/lib/x86_64-linux-gnu/libpthread.a -lgtest_main /usr/lib/x86_64-linux-gnu/libgtest.a
