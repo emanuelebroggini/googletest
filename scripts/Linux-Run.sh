87ce96da5a2d0ae6dbe30fe05d768dce52d69308
AndreaOrtenzi <andre.orto@tiscali.it>
2023-06-04 13:34:59 +0200

    Check if file exists in run.sh

--- scripts/Linux-Run.sh
+++ scripts/Linux-Run.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
-for file in ./googletest/samples/sample*.exe; do
-    "$file"              # Run the executable
-    exit_code=$?         # Store the exit code in a variable
+for file in ./googletest/samples/*.exe; do
+    if [ -f $file ]; then    # Check if the file exists
+        "$file"              # Run the executable
+        exit_code=$?         # Store the exit code in a variable
 
-    if [ $exit_code -ne 0 ]; then
-        echo "Error: $file returned a non-zero exit code: $exit_code"
-        exit 1
+        if [ $exit_code -ne 0 ]; then
+            echo "Error: $file returned a non-zero exit code: $exit_code"
+            exit 1
+        fi
     fi
 done
 
