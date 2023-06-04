2f53c792eb65c2766b311951163289980b34cee0
AndreaOrtenzi <andre.orto@tiscali.it>
2023-06-04 13:30:39 +0200

    Don't check if .exe is executable

--- scripts/Linux-Run.sh
+++ scripts/Linux-Run.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 for file in ./googletest/samples/sample*.exe; do
-    if [ -x "$file" ]; then  # Check if the file is executable
-        "$file"              # Run the executable
-        exit_code=$?         # Store the exit code in a variable
+    "$file"              # Run the executable
+    exit_code=$?         # Store the exit code in a variable
 
-        if [ $exit_code -ne 0 ]; then
-            echo "Error: $file returned a non-zero exit code: $exit_code"
-            exit 1
-        fi
+    if [ $exit_code -ne 0 ]; then
+        echo "Error: $file returned a non-zero exit code: $exit_code"
+        exit 1
     fi
 done
 
