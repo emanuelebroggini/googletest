5226aa5cdbad7ff3c4128eabd7c0a1278e79b627
AndreaOrtenzi <andre.orto@tiscali.it>
2023-06-04 13:45:57 +0200

    Back to for loop in run.sh without checking exit code

--- scripts/Linux-Run.sh
+++ scripts/Linux-Run.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-cd googletest/samples/ && ./sample10.exe && ./sample2.exe && ./sample4.exe && ./sample9.exe && ./samples135.exe && ./samples678.exe
+for file in ./googletest/samples/*.exe; do
+    if [ -f $file ]; then    # Check if the file exists
+        "$file"              # Run the executable
+    fi
+done
 
