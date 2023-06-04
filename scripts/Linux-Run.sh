225ea4d62a4dec6b8f9501b60d6a83d0b7b89269
AndreaOrtenzi <andre.orto@tiscali.it>
2023-06-04 13:37:32 +0200

    Execute tests one by one

--- scripts/Linux-Run.sh
+++ scripts/Linux-Run.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
-for file in ./googletest/samples/*.exe; do
-    if [ -f $file ]; then    # Check if the file exists
-        "$file"              # Run the executable
-        exit_code=$?         # Store the exit code in a variable
-
-        if [ $exit_code -ne 0 ]; then
-            echo "Error: $file returned a non-zero exit code: $exit_code"
-            exit 1
-        fi
-    fi
-done
+./googletest/samples/sample10.exe
+./googletest/samples/sample2.exe
+./googletest/samples/sample4.exe
+./googletest/samples/sample9.exe
+./googletest/samples/samples.exe
+./googletest/samples/samples135.exe
+./googletest/samples/samples678.exe
 
