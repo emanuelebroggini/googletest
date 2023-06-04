ed8b71de6dba87ec8c3b37afaa5119a955320373
AndreaOrtenzi <andre.orto@tiscali.it>
2023-06-04 13:49:18 +0200

    Run file without ""

--- scripts/Linux-Run.sh
+++ scripts/Linux-Run.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 for file in ./googletest/samples/*.exe; do
     if [ -f $file ]; then    # Check if the file exists
-        "$file"              # Run the executable
+        $file              # Run the executable
+        
     fi
 done
 
